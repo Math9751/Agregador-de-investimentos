741504e67aecb91e937ea08a8418b901b812f7d5
Math9751 <matthewbelt.mc@gmail.com>
2025-10-15 14:53:41 -0300

    aprimorando testes unitários tanto pra cenários positivos qnt negativos

--- src/test/java/tech/math/agregadorinvestimentos/service/UserServiceTest.java
+++ src/test/java/tech/math/agregadorinvestimentos/service/UserServiceTest.java
@@ -1,11 +1,27 @@
 package tech.math.agregadorinvestimentos.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
+
+import java.time.Instant;
+import java.util.UUID;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import tech.math.agregadorinvestimentos.controller.CreateUserDto;
+import tech.math.agregadorinvestimentos.entity.User;
 import tech.math.agregadorinvestimentos.repository.UserRepository;
 
 @ExtendWith(MockitoExtension.class)
@@ -17,28 +33,54 @@ public class UserServiceTest {
     @InjectMocks
     private UserService userService;
 
-    @Test
-    void testCreateUser() {
+    @Captor
+    private ArgumentCaptor<User> userArgumentCaptor;
 
-    }
+    @Nested
+    class CreateUser {
+        @Test
+        @DisplayName("Deve criar um novo usuário com sucesso")
+        void shouldCreateUserWithSuccess() {
+            // Arrange
+            var user = new User (
+                UUID.randomUUID(),
+                "username",
+                "email@email.com",
+                "password",
+                Instant.now(),
+                null
+            );
+            doReturn(user).when(userRepository).save(userArgumentCaptor.capture());
+            var input = new CreateUserDto(
+                "username",
+                "email@email.com",
+                "password"
+            );
 
-    @Test
-    void testDeleteUser() {
+            // Act
+            var output = userService.createUser(input);
+            // Assert
+            assertNotNull(output);
+            var capturedUser = userArgumentCaptor.getValue();
+            assertEquals(input.username(), capturedUser.getUsername());
+            assertEquals(input.email(), capturedUser.getEmail());
+            assertEquals(input.password(), capturedUser.getPassword());
+        }
 
-    }
+        @Test
+        @DisplayName("Should throw exception when error occurs")
+        void shouldThrowExceptionWhenErrorOccurs() {
+            doThrow(new RuntimeException()).when(userRepository).save(any());
+            var input = new CreateUserDto(
+                "username",
+                "email@email.com",
+                "password"
+            );
 
-    @Test
-    void testGetAllUsers() {
-
-    }
-
-    @Test
-    void testGetUserById() {
-
-    }
-
-    @Test
-    void testUpdateUserById() {
-
+            // Act & Assert
+            assertThrows(RuntimeException.class, () -> {
+                userService.createUser(input);
+            });
+        }
     }
 }
